576a17e80d4118c0ca0620edfb015b413b7f41a6
Peyton Wang <peyton.wang@wellesley.edu>
2021-01-28 14:54:15 -0800

    Added screens

--- App.js
+++ App.js
@@ -3,129 +3,199 @@ import { StyleSheet, Button, View, Text, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-function HomeScreen({ navigation }) {
-	return (
-		<View style={styles.home}>
-			<Button
-				title="Take Action Now!"
-				onPress={() => navigation.navigate('Details')}
-			/>
-			<Button
-				title="Polls"
-				onPress={() => navigation.navigate('Details')}
-			/>
-			<Button
-				title="Forum"
-				onPress={() => navigation.navigate('Details')}
-			/>
-			<Button
-				title="Websites"
-				onPress={() => navigation.navigate('Details')}
-			/>
-			<Button
-				title="About Us"
-				onPress={() => navigation.navigate('Details')}
-			/>
-			<Button
-				title="Contact Us"
-				onPress={() => navigation.navigate('Details')}
-			/>
-		</View>
-	);
-}
+/* Home Screen */
+	function HomeScreen({ navigation }) {
+		return (
+			<View style={styles.home}>
+				<Button
+					title="Take Action Now!"
+					onPress={() => navigation.navigate('Take Action Now')}
+				/>
+				<Button
+					title="Polls"
+					onPress={() => navigation.navigate('Polls')}
+				/>
+				<Button
+					title="Forum"
+					onPress={() => navigation.navigate('Forum')}
+				/>
+				<Button
+					title="Websites"
+					onPress={() => navigation.navigate('Websites')}
+				/>
+				<Button
+					title="About Us"
+					onPress={() => navigation.navigate('About Us')}
+				/>
+				<Button
+					title="Contact Us"
+					onPress={() => navigation.navigate('Contact Us')}
+				/>
+			</View>
+		);
+	}
 
-function LogoTitle() {
-	return (
-		<Image
-			style={styles.logo}
-			source={require('./assets/logo.png')}
-		/>
-	);
-}
+	/* Logo */
+		function LogoTitle() {
+			return (
+				<Image
+					style={styles.logo}
+					source={require('./assets/logo.png')}
+				/>
+			);
+		}
 
-function ProfileScreen({ navigation }) {
-	return (
-		<View style={styles.details}>
-		</View>
-	);
-}
+	/* Features */
+		function TakeActionNowScreen({ navigation }) {
+			return (
+				<View style={styles.features}>
+					<Text>Take Action Now Screen</Text>
+				</View>
+			);
+		}
+		function PollsScreen({ navigation }) {
+			return (
+				<View style={styles.features}>
+					<Text>Polls Screen</Text>
+				</View>
+			);
+		}
+		function ForumScreen({ navigation }) {
+			return (
+				<View style={styles.features}>
+					<Text>Forum Screen</Text>
+				</View>
+			);
+		}
+		function WebsitesScreen({ navigation }) {
+			return (
+				<View style={styles.features}>
+					<Text>Websites Screen</Text>
+				</View>
+			);
+		}
+		function AboutUsScreen({ navigation }) {
+			return (
+				<View style={styles.features}>
+					<Text>About Us Screen</Text>
+				</View>
+			);
+		}
+		function ContactUsScreen({ navigation }) {
+			return (
+				<View style={styles.features}>
+					<Text>Contact Us Screen</Text>
+				</View>
+			);
+		}
 
-function SettingsScreen({ navigation }) {
-	return (
-		<View style={styles.details}>
-			
-		</View>
-	);
-}
+/* Profile */
+	function ProfileScreen({ navigation }) {
+		return (
+			<View style={styles.features}>
+			</View>
+		);
+	}
 
-function DetailsScreen({ navigation }) {
-	return (
-		<View style={styles.details}>
-			<Text>Details Screen</Text>
-			<Button
-				title="Go to Details... again"
-				onPress={() => navigation.push('Details')}
-			/>
-		</View>
-	);
-}
+/* Settings */
+	function SettingsScreen({ navigation }) {
+		return (
+			<View style={styles.features}>
+
+			</View>
+		);
+	}
+
 const Tab = createBottomTabNavigator();
-const SettingsStack = createStackNavigator();
 const HomeStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+const SettingsStack = createStackNavigator();
 
-function App() {
-	return (
-		<NavigationContainer>
-			<Tab.Navigator>
-				<Tab.Screen name="Home">
-					{() => (
-						<HomeStack.Navigator>
-							<HomeStack.Screen 
-								name="Home" 
-								component={HomeScreen} 
-								options={{ headerTitle: props => <LogoTitle {...props} /> }}
-							/>
-							<HomeStack.Screen name="Details" component={DetailsScreen} />
-						</HomeStack.Navigator>
-					)}
-				</Tab.Screen>
-				<Tab.Screen name="Profile">
-					{() => (
-						<SettingsStack.Navigator>
-							<SettingsStack.Screen name="Profile" component={ProfileScreen} />
-						</SettingsStack.Navigator>
-					)}
-				</Tab.Screen>
-				<Tab.Screen name="Settings">
-					{() => (
-						<SettingsStack.Navigator>
-							<SettingsStack.Screen name="Settings" component={SettingsScreen} />
-						</SettingsStack.Navigator>
-					)}
-				</Tab.Screen>
-			</Tab.Navigator>
-		</NavigationContainer>
-	);
-}
+/* App */
+	function App() {
+		return (
+			<NavigationContainer>
+				<Tab.Navigator>
+					<Tab.Screen
+						name="Home"
+						options={{
+							tabBarLabel: 'Home',
+							tabBarIcon: ({ color, size }) => (
+								<MaterialCommunityIcons name="home" color={color} size={size} />
+							),
+						}}
+					>
+						{() => (
+							<HomeStack.Navigator>
+								<HomeStack.Screen
+									name="Home"
+									component={HomeScreen}
+									options={{ headerTitle: props => <LogoTitle {...props} /> }}
+								/>
+								<HomeStack.Screen name="Take Action Now" component={TakeActionNowScreen} />
+								<HomeStack.Screen name="Polls" component={PollsScreen} />
+								<HomeStack.Screen name="Forum" component={ForumScreen} />
+								<HomeStack.Screen name="Websites" component={WebsitesScreen} />
+								<HomeStack.Screen name="About Us" component={AboutUsScreen} />
+								<HomeStack.Screen name="Contact Us" component={ContactUsScreen} />
+							</HomeStack.Navigator>
+						)}
+					</Tab.Screen>
+					<Tab.Screen 
+						name="Profile"
+						options={{
+							tabBarLabel: 'Profile',
+							tabBarIcon: ({ color, size }) => (
+								<MaterialCommunityIcons name="account" color={color} size={size} />
+							),
+						}}
+					>
+						{() => (
+							<ProfileStack.Navigator>
+								<ProfileStack.Screen name="Profile" component={ProfileScreen} />
+							</ProfileStack.Navigator>
+						)}
+					</Tab.Screen>
+					<Tab.Screen 
+						name="Settings"
+						options={{
+							tabBarLabel: 'Settings',
+							tabBarIcon: ({ color, size }) => (
+								<MaterialCommunityIcons name="cog" color={color} size={size} />
+							),
+						}}
+						>
+						{() => (
+							<SettingsStack.Navigator>
+								<SettingsStack.Screen name="Settings" component={SettingsScreen} />
+							</SettingsStack.Navigator>
+						)}
+					</Tab.Screen>
+				</Tab.Navigator>
+			</NavigationContainer>
+		);
+	}
 
-export default App;
+	export default App;
 
-const styles = StyleSheet.create({
-	home: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: '#FC9C73'
-	},
-	logo: {
-		width: 50,
-		height: 50
-	},
-	details: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: '#FC9C73'
-	},
-});
+/* Styles */
+	const styles = StyleSheet.create({
+		home: {
+			flex: 1,
+			alignItems: 'center',
+			justifyContent: 'center',
+			backgroundColor: '#FC9C73'
+		},
+		logo: {
+			width: 40,
+			height: 40
+		},
+		features: {
+			flex: 1,
+			alignItems: 'center',
+			justifyContent: 'center',
+			backgroundColor: '#FC9C73'
+		},
+	});
