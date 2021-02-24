f819803aa717b4cb37818cb1b4dab7250f32dfd2
Peyton Wang <peyton.wang@wellesley.edu>
2021-02-23 22:31:34 -0800

    Updated styles

---
+++ styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    home: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FC9C73'
+    },
+    logo: {
+        width: 40,
+        height: 40
+    },
+    features: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FC9C73'
+    },
+})
